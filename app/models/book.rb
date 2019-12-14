f83c3f122ebafc9b08788c3d168f0b9bc61a4f65
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-14 14:29:09 -0700

    Book and author models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,14 @@
 class Book < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :publication_year
+  validates_presence_of :pages
+
+  has_many :book_authors
+  has_many :authors, through: :book_authors
+
+  def find_authors
+    authors.map do |author|
+      author.name
+    end
+  end
 end
