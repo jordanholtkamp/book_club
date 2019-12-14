7b0ef65cfe0b4d7d1b33451d90cf44f300cf17d8
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-14 14:57:28 -0700

    Write test for author show page

---
+++ spec/features/authors/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'As a user' do
+  describe 'When I visit the author show page' do
+    it "shows all the details of the author" do
+      tom = Author.create!(name: 'Tom Robbins')
+      jitter = Book.create!(title: 'Jitterbug Perfume', publication_year: '1985', pages: 325)
+      skinny_legs = Book.create!(title: 'Skinny Legs and All', publication_year: '1991', pages: 472)
+      tom.books << jitter
+      tom.books << skinny_legs
+
+      visit "/authors/#{tom.id}"
+
+      expect(page).to have_content(tom.name)
+      expect(page).to have_content(jitter.title)
+      expect(page).to have_content(skinny_legs.title)
+      expect(page).to have_content(399)
+    end
+  end
+end
