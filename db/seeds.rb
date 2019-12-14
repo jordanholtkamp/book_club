fb71f25b3dba8d62127a175822539bae893f88bb
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-14 14:28:41 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+tom = Author.create!(name: 'Tom Robbins')
+kurt = Author.create!(name: 'Kurt Vonnegut')
+john = Author.create!(name: 'John Steinbeck')
+
+jitter = Book.create!(title: 'Jitterbug Perfume', publication_year: '1985', pages: 325)
+tom.books << jitter
+john.books << jitter
+
+slaughterhouse = Book.create!(title: 'Salughterhouse Five', publication_year: '1978', pages: 286)
+kurt.books << slaughterhouse
