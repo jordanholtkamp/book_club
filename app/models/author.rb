e4a4e1f4999d9990932edebbf5fc96db0be1e850
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-14 15:06:28 -0700

    Add average pages model method

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,10 @@
 class Author < ApplicationRecord
   validates_presence_of :name
+
   has_many :book_authors
   has_many :books, through: :book_authors
+
+  def average_pages
+    books.average(:pages).to_i
+  end
 end
