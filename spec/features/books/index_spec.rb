dd9cafec976ece77626e1073922c02c73a9617ae
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-14 14:57:19 -0700

    Take out comments

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -53,13 +53,5 @@ describe 'As a user', type: :feature do
 
       expect(current_path).to eq("/authors/#{@kurt.id}")
     end
-    # As a user,
-      # When I visit the Book Index Page,
-      #   Then I see each author's name is a link
-      # When I click the link
-      #   Then I am taken to an Author Show Page where I see the author with that id including the author's:
-      #   - name
-      #   - the title of each book they have written
-      #   - the average number of pages for all of their books
   end
 end
