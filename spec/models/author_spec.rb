090ff8fa429292e25a4ae286d7fecce6c5d59661
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-14 15:06:00 -0700

    Add test for model method

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,18 @@ describe Author, type: :model do
     it { should have_many :book_authors }
     it { should have_many(:books).through(:book_authors) }
   end
+
+  describe 'model methods' do
+    describe '#average_pages' do
+      it "returns average pages per book for an author" do
+        tom = Author.create!(name: 'Tom Robbins')
+        jitter = Book.create!(title: 'Jitterbug Perfume', publication_year: '1985', pages: 325)
+        skinny_legs = Book.create!(title: 'Skinny Legs and All', publication_year: '1991', pages: 472)
+        tom.books << jitter
+        tom.books << skinny_legs
+
+        expect(tom.average_pages).to eq(398)
+      end
+    end
+  end
 end
